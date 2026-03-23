588c0e2541d50b1330e783c26762f8efe5d13e99
kfatima8162 <fatimakashifa04@gmail.com>
2026-03-23 14:28:56 +0530

    completed expense tracker

--- personal_expense_tracker/application/urls.py
+++ personal_expense_tracker/application/urls.py
@@ -1,8 +1,14 @@
 from django.urls import path
-from application.views import register,login,home
+from application.views import register,login,home,logout_view,add_expense,add_income,delete_expense
 
 urlpatterns = [
-    path('',register,name='register'),
-    path('login/',login,name='login'),
-    path('home/',home,name='home')
-]
+    path('home/',home,name='home'),
+
+path('login/',login,name='login'),
+path('',register,name='register'),
+path('logout/',logout_view,name='logout'),
+
+path('add-expense/',add_expense,name='add_expense'),
+path('add-income/',add_income,name='add_income'),
+path('delete-expense/<int:id>/',delete_expense,name='delete_expense'),
+    ]
--- personal_expense_tracker/application/views.py
+++ personal_expense_tracker/application/views.py
@@ -1,7 +1,8 @@
 from django.shortcuts import render,redirect
 from django.contrib.auth.models import User
-from django.contrib.auth import login, authenticate
+from django.contrib.auth import login, authenticate,logout
 from django.contrib.auth.decorators import login_required
+from .models import Expense, Income
 # Create your views here.
 def register(request):
     if request.method == 'POST':
@@ -15,20 +16,104 @@ def register(request):
 
 def login(request):
     if request== 'POST':
-        username = request.POST.get('username')
-        password = request.POST.get('password')
+        username = request.POST('username')
+        password = request.POST('password')
         user = authenticate(request, username=username, password=password)
         if user is not None:
             login(request, user)
             return redirect('home')
-    return render(request, 'home.html')
+    return render(request, 'login.html')
+
 @login_required
 def home(request):
-    if request== 'POST':
-        username = request.POST.get('username')
-        password = request.POST.get('password')
-        user = authenticate(request, username=username, password=password)
-        if user is not None:
-            login(request, user)
-            return redirect('home')
-    return render(request,'home.html')
+
+    expenses = Expense.objects.filter(user=request.user)
+    income = Income.objects.filter(user=request.user)
+
+    total_expense = sum(exp.amount for exp in expenses)
+    total_income = sum(inc.amount for inc in income)
+
+    balance = total_income - total_expense
+
+    context = {
+        'expenses': expenses,
+        'income': income,
+        'total_expense': total_expense,
+        'total_income': total_income,
+        'balance': balance
+    }
+
+    return render(request, 'home.html', context)
+
+def logout_view(request):
+    logout(request)
+    return redirect('login')
+
+# ADD EXPENSE
+@login_required
+def add_expense(request):
+
+    if request.method == "POST":
+
+        title = request.POST.get('title')
+        amount = float(request.POST.get('amount'))
+        category = request.POST.get('category')
+        date = request.POST.get('date')
+
+        expense = Expense(
+            user=request.user,
+            title=title,
+            amount=amount,
+            category=category,
+            date=date
+        )
+
+        expense.save()
+
+        return redirect('Home')
+
+    return render(request, 'expense.html')
+
+
+# ADD INCOME
+@login_required
+def add_income(request):
+
+    if request.method == "POST":
+
+        source = request.POST.get('source')
+        amount = request.POST.get('amount')
+        date = request.POST.get('date')
+
+        income = Income(
+            user=request.user,
+            source=source,
+            amount=amount,
+            date=date
+        )
+
+        income.save()
+
+        return redirect('Home')
+
+    return render(request, 'income.html')
+
+
+# DISPLAY EXPENSES
+@login_required
+def expense_display(request):
+
+    expenses = Expense.objects.filter(user=request.user)
+
+    return render(request, 'expense_display.html', {'expenses': expenses})
+
+
+# DELETE EXPENSE
+@login_required
+def delete_expense(request, id):
+
+    expense = Expense.objects.get(pk=id, user=request.user)
+
+    expense.delete()
+
+    return redirect('Home')
